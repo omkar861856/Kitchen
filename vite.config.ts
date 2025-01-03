bc52d6e0a2e2c2cece7420404a03e3f1275d946d
Omkar Lolge <omkarlolge@MacBookAir.lan>
2025-01-03 16:09:02 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths are used for assets
-  build: {
-    outDir: 'dist', // Output directory for production build
-    assetsDir: 'assets', // Folder for static assets (CSS, JS, etc.)
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]', // Ensures hashed file names in assets folder
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
-  server: {
-    open: true, // Automatically open the browser on server start
-  },
-});
+})
